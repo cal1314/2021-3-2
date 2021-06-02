caa664f71db8c39875c9d6e9f2d1289b5a4fea1f
cal1314 <34121217+cal1314@users.noreply.github.com>
2021-06-03 01:14:51 +0800

    Update toString.php
    xxx

--- toString.php
+++ toString.php
@@ -3,7 +3,7 @@ class Account{
     public $user = 1;
     private $pwd = 2;
 
-    //自定义的格式化输出方法
+    //自定义的格式化输出方法 xx
     public function __toString(){
         return "当前对象的用户名是$this->user,密码是$this->user";
     }
